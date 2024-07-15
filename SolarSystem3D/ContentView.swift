ef835ae69300d2316847243fc4e22d244065f90f
Julio César Fernández <juliocesar.fernandez@gabhel.com>
2024-07-15 16:18:24 +0200

    Initial Commit

---
+++ SolarSystem3D/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SolarSystem3D
+//
+//  Created by Julio César Fernández Muñoz on 15/7/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ SolarSystem3D/SolarSystem3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSystem3DApp.swift
+//  SolarSystem3D
+//
+//  Created by Julio César Fernández Muñoz on 15/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SolarSystem3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
